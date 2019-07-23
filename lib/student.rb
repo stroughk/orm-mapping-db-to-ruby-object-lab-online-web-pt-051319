f184f94bae012615a73861825d39302172052dd8
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-23 22:10:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,13 +12,13 @@ class Student
 
   def self.all
     sql =  <<- SQL
-    SELECT * 
+    SELECT *
     FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)  
-    end    
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
